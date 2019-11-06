06eff9711f868938a01b08c843711219c5c36055
rjones17 <jones.rebekah17@gmail.com>
2019-11-05 19:39:30 -0600

    linking heroku

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,20 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(`/metadata/${sample}`).then((data) => {
+    
+  // Use d3 to select the panel with id of `#sample-metadata`
+  var PANEL = d3.select("#sample-metadata");
 
-    // Use `.html("") to clear any existing metadata
+  // Use `.html("") to clear any existing metadata
+  PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+  Object.entries(data).forEach(([key, value]) => {
+    PANEL.append("h6").text(`${key}: ${value}`)
 
 function buildCharts(sample) {
 
